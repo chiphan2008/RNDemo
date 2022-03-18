4c6d2578eb3de06ba454f68dec17df08c7935da4
Jimmy Phan <chiph@zen-s.com>
2022-03-19 06:46:22 +0700

    Update ruule for pre-commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,5 +14,13 @@ module.exports = {
     'react-native/no-color-literals': 2,
     'react-native/no-raw-text': 2,
     'react-native/no-single-element-style-arrays': 2,
+    'no-console': ['warn', {allow: ['info', 'warn', 'error']}],
+    'no-plusplus': 0,
+    'prefer-destructuring': ['warn', {object: true, array: false}],
+    'no-underscore-dangle': 0,
+    'react-hooks/exhaustive-deps': 0,
+    radix: 0,
+    'import/no-extraneous-dependencies': 0,
+    'jsx-a11y/media-has-caption': 0,
   },
 };
